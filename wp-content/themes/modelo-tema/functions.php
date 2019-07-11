4c2dd7e998d988e48c756484f9a0aa0ae60044a5
fgeanlopes <f.geanlopes@gmail.com>
2019-07-11 09:57:09 -0300

    Tradução das frases do contact form 7

--- wp-content/themes/modelo-tema/functions.php
+++ wp-content/themes/modelo-tema/functions.php
@@ -1,24 +1,25 @@
 <?php
+add_theme_support( 'post-thumbnails', array( 'post'));
 
 function wpdocs_remove_menus(){
-  //add_filter('show_admin_bar', '__return_false');   // remove barra superior wordpress
-  //remove_menu_page( 'index.php' );                  //Dashboard
-  //remove_menu_page( 'jetpack' );                    //Jetpack* 
-  //remove_menu_page( 'edit.php' );                   //Posts
-  //remove_menu_page( 'upload.php' );                 //Media
-  //remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  add_filter('show_admin_bar', '__return_false');   // remove barra superior wordpress
+  remove_menu_page( 'index.php' );                  //Dashboard
+  remove_menu_page( 'jetpack' );                    //Jetpack* 
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'upload.php' );                 //Media
+  remove_menu_page( 'edit.php?post_type=page' );    //Pages
   remove_menu_page( 'edit-comments.php' );          //Comments
-  //remove_menu_page( 'themes.php' );                 //Appearance
-  //remove_menu_page( 'plugins.php' );                //Plugins
+  remove_menu_page( 'themes.php' );                 //Appearance
+  remove_menu_page( 'plugins.php' );                //Plugins
   //remove_menu_page( 'users.php' );                //Users
   remove_menu_page( 'tools.php' );                  //Tools
   //remove_menu_page( 'options-general.php' );      //Settings
   remove_menu_page( 'edit.php?post_type=acf-field-group' );		// remove acf menu
   remove_menu_page( 'wpcf7' );						// remove contact form menu
 }
-add_action( 'admin_menu', 'wpdocs_remove_menus' );
+//add_action( 'admin_menu', 'wpdocs_remove_menus' );
 
-add_theme_support( 'post-thumbnails', array( 'post'));
+add_theme_support( 'post-thumbnails' );
 
 //DEFININDO TAMANHO DO RESUMO POST
 add_filter('excerpt_length', function ($length) {
@@ -71,7 +72,7 @@ add_action('init', 'produtos');
 
 
 // Register Custom Taxonomy
-function cat_produtos(){
+function categoria_de_produtos(){
   $labels = array(
       'name' => _x('Categoria de produto', 'Taxonomy General Name', 'text_domain'),
       'singular_name' => _x('Categoria de produto', 'Taxonomy Singular Name', 'text_domain'),
@@ -94,12 +95,12 @@ function cat_produtos(){
       'show_tagcloud' => true,
       'rewrite' => true,
       'rewrite' => array (
-          'slug'=>'categoria-DE-produtos'
+          'slug'=>'produtos'
       )
   );
   
-  register_taxonomy('cat_produtos', array('produtos'), $args);
+  register_taxonomy('categoria_de_produtos', array('produtos'), $args);
 }
-add_action('init', 'cat_produtos');
+add_action('init', 'categoria_de_produtos');
 
 ?>
